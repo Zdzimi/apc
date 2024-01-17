95651d80d3e934f63ae9d4ce4af90cf340553d7a
Michal <michalzdziennickip@gmail.com>
2024-01-17 18:54:58 +0100

    Initial commit

---
+++ src/test/java/com/zdzimi/apc/ApcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zdzimi.apc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApcApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
